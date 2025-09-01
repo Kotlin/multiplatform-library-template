01a619c0c171714d63c68eba2168fa926a9d1789
pahill <pamela.a.hill@gmail.com>
2025-09-01 17:35:14 +0200

    Update dependencies: AGP, Kotlin, Gradle, and Maven Publish plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -52,7 +51,7 @@ android {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
 
     signAllPublications()
 
